2c9fe539151cd3af3a5e81847b52fcc203fccdae
Bhupender Singh <bhupendersinghbhauriyal@gmail.com>
2021-09-03 20:46:13 +0530

    media queries added

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,13 @@
 import logo from './cafe-central-logo.png'
+import menulogo from './menu.png'
 import React from 'react'
 import './Navbar.css'
 
 function Navbar() {
   return (
     <nav>
-      <img src={logo} alt="main logo" />
+      <img src={logo} alt="main logo" id="logo" />
+      <img src={menulogo} alt="main logo" id="menu" />
       <ul>
         <li>HOME</li>
         <li>ABOUT</li>
